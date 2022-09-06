c3d5c9fb73546a7ad2fbddc5e8fd8da3b89251cc
Brooke Ross <brooke.ross2800@gmail.com>
2022-09-06 07:47:11 -0600

    commit tree a2866799bd780df0808bf519a1f5c8f9301dd60d

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(names,looking){
+    return names.filter(
+        (possible)=>possible.toLowerCase()===looking.toLowerCase()
+    );
+}
+
+function fuzzyMatch(driver,match){
+    return driver.filter(
+        (possible)=>possible.toLowerCase().indexOf(match.toLowerCase())===0
+    );
+}
+
+function matchName(data,lookingName){
+    return data.filter((record)=>record.name===lookingName);
+}
